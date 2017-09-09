bfe05c5f53c03585d6c06eca35f6758996f6749f
Victor Allen <vamuigua@gmail.com>
2017-09-09 18:03:26 +0300

    Added for loop to loop through GetGuessAndPrintBack function

---
+++ BullCowGame.cpp
@@ -0,0 +1,42 @@
+//Preprocessor Directive
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Prototypes
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+//entry point of ours app
+int main()
+{
+	PrintIntro();
+
+	//Loop for the number of turns asking for guesses
+	const int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		GetGuessAndPrintBack();
+		cout << endl;
+	}
+}
+
+//introduce the game
+void PrintIntro() {
+	const int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+	return;
+}
+
+//get the guess of the user
+string GetGuessAndPrintBack() {
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	//print the guess back
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
+}
