c21173bd52c21f89e3035e5b935394177ebb5dab
Victor Allen <vamuigua@gmail.com>
2017-09-09 19:24:52 +0300

    Added do while loop to allow the user to play game again

--- main.cpp
+++ main.cpp
@@ -13,9 +13,16 @@ bool AskToPlayAgain();
 //entry point of ours app
 int main()
 {
-	PrintIntro();
-	PlayGame();
-	AskToPlayAgain();
+	bool bPlayAgain = false;
+
+	do
+	{
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	}
+	while (bPlayAgain);
+
 	return 0; //exit the application/game
 }
 
@@ -51,8 +58,8 @@ string GetGuess()
 
 bool AskToPlayAgain()
 {
-	cout <<  "Do you want to play again?";
+	cout <<  "Do you want to play again? (y/n)";
 	string Response = "";
-	getline(cin,Response);
+	getline(cin, Response);
 	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
