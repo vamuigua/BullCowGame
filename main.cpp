08d17b46b05028ce9932921bda409e81221c795d
Victor Allen <vamuigua@gmail.com>
2017-09-09 18:52:43 +0300

    Removed duplicate cpp file Modified layout to be more clear

--- main.cpp
+++ main.cpp
@@ -6,21 +6,20 @@ using namespace std;
 
 //Prototypes
 void PrintIntro();
-string GetGuessAndPrintBack();
+void PlayGame();
+string GetGuess();
 
 //entry point of ours app
 int main()
 {
 	PrintIntro();
-
-	GetGuessAndPrintBack();
-	GetGuessAndPrintBack();
-
-	cout << endl;
+	PlayGame();
+	return 0; //exit the application/game
 }
 
-//introduce the game
-void PrintIntro() {
+//introducing the game
+void PrintIntro()
+{
 	const int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
@@ -29,12 +28,21 @@ void PrintIntro() {
 	return;
 }
 
-//get the guess of the user
-string GetGuessAndPrintBack() {
+void PlayGame()
+{
+	//Loop for the number of turns asking for guesses
+	const int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++) {
+		string Guess = GetGuess();
+		cout << "Your guess was: " << Guess << endl;
+		cout << endl;
+	}
+}
+
+string GetGuess()
+{
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	//print the guess back
-	cout << "Your guess was: " << Guess << endl;
 	return Guess;
 }
